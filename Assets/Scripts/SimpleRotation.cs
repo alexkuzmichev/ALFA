4d4b073bbfd438c6cb79a384379bc91036ed27a1
Alex Kuzmichev <kuzmichov@gmail.com>
2018-11-12 01:08:04 +0300

    Sputnik model

---
+++ Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime*10, Space.World);
+    }
+}
